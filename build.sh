7ffb2ff2c1c8b2cea818a19d6ba16f9403ae2de7
rayuhhh <rayuhhh@gmail.com>
2025-07-30 18:59:13 -0500

    setting up for github pages

---
+++ build.sh
@@ -0,0 +1,1 @@
+python3 src/main.py "https://github.com/rayuhhh/static_site_generator"
--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from textnode import TextNode
+import sys
 import os
 import shutil
 import logging
@@ -17,7 +18,7 @@ def extract_title(markdown):
             return line.strip()[2:].strip()
     raise Exception("No header present")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from: {from_path} to {dest_path} using {template_path}")
     file_dict = {}
 
@@ -33,6 +34,8 @@ def generate_page(from_path, template_path, dest_path):
 
     file_dict[template_path] = file_dict[template_path].replace("{{ Title }}", title)
     file_dict[template_path] = file_dict[template_path].replace("{{ Content }}", html_string)
+    file_dict[template_path] = file_dict[template_path].replace('href="/', f'href="{basepath}')
+    file_dict[template_path] = file_dict[template_path].replace('src="/', f'src="{basepath}')
 
     dest_dir_path = os.path.dirname(dest_path)
     if dest_dir_path != "":
@@ -43,8 +46,8 @@ def generate_page(from_path, template_path, dest_path):
     #     f.write(file_dict[template_path])
     
 
-def generate_page_recursive(dir_path_content, template_path, dest_dir_path):
-    ## crawl every entry in content dir
+def generate_page_recursive(dir_path_content, template_path, dest_dir_path, basepath):
+
     list_dir = os.listdir(dir_path_content)
 
     for item in list_dir:
@@ -52,41 +55,29 @@ def generate_page_recursive(dir_path_content, template_path, dest_dir_path):
         dest_path = os.path.join(dest_dir_path, item)
         if os.path.isfile(from_path):
             dest_path = Path(dest_path).with_suffix(".html")
-            generate_page(from_path, template_path, dest_path)
+            generate_page(from_path, template_path, dest_path, basepath)
         else:
-            generate_page_recursive(from_path, template_path, dest_path)
+            generate_page_recursive(from_path, template_path, dest_path, basepath)
 
-    # try:
-    #     with open(from_path, "r") as file:
-    #         content = file.read()
-    # except FileNotFoundError:
-    #     print(f"Error: file: {from_path} not found")
-    # except Exception as e:
-    #     print(f"An error: {e} has occured")
 
 
         
-
-
 def main():
-    ### writing recursive func copies all contents from source dir to a destination dir
-    # our case: static to public
-
-    # first delete all contents of destination dir (public)
-    # copy all files and subdirs, nested files, etc
+    basepath = "/"
+    if len(sys.argv) == 2:
+        basepath = sys.argv[1]
     template = "/home/rayn/workspace/github.com/rayuhhh/static_site_generator/template.html"
-    # content = "/home/rayn/workspace/github.com/rayuhhh/static_site_generator/content/index.md"
     source = "/home/rayn/workspace/github.com/rayuhhh/static_site_generator/static"
-    destination = "/home/rayn/workspace/github.com/rayuhhh/static_site_generator/public"
+    destination = "/home/rayn/workspace/github.com/rayuhhh/static_site_generator/docs"
     dest = "/home/rayn/workspace/github.com/rayuhhh/static_site_generator/public/index.html"
     content = "/home/rayn/workspace/github.com/rayuhhh/static_site_generator/content"
     
-    logging.info(f"hello")
+    # logging.info(f"hello")
     copy_dir_contents_clean(source, destination)
 
     #### changing generate_page to gen_page_recursive
     #generate_page(content, template, dest)
-    generate_page_recursive(content, template, destination)
+    generate_page_recursive(content, template, destination, basepath)
 
     # print("\n--- Verification ---")
     # print(f"Contents of 'public_test': {os.listdir('../public_test')}")
@@ -97,29 +88,30 @@ def main():
 
 
 if __name__ == "__main__":
+    main()
     # Create dummy source and destination directories for testing
-    if os.path.exists("../static_test"):
-        shutil.rmtree("../static_test") # Ensure a fresh start for the test source.
-    os.makedirs("../static_test/img")
-    os.makedirs("../static_test/sub")
-    os.makedirs("../static_test/sub/nested_sub")
-    os.makedirs("../static_test/css")
-    with open("../static_test/index.html", "w") as f:
-        f.write("<html><body><h1>Hello from static!</h1></body></html>")
-    with open("../static_test/css/style.css", "w") as f:
-        f.write("body { background-color: lightblue; }")
-    with open("../static_test/img/logo.png", "w") as f:
-        f.write("This is a dummy image file content.")
-    with open("../static_test/sub/test.txt", "w") as f:
-        f.write("A file in a subdirectory.")
-    with open("../static_test/sub/nested_sub/deep.txt", "w") as f:
-        f.write("A file in a deeply nested subdirectory.")
+    # if os.path.exists("../static_test"):
+    #     shutil.rmtree("../static_test") # Ensure a fresh start for the test source.
+    # os.makedirs("../static_test/img")
+    # os.makedirs("../static_test/sub")
+    # os.makedirs("../static_test/sub/nested_sub")
+    # os.makedirs("../static_test/css")
+    # with open("../static_test/index.html", "w") as f:
+    #     f.write("<html><body><h1>Hello from static!</h1></body></html>")
+    # with open("../static_test/css/style.css", "w") as f:
+    #     f.write("body { background-color: lightblue; }")
+    # with open("../static_test/img/logo.png", "w") as f:
+    #     f.write("This is a dummy image file content.")
+    # with open("../static_test/sub/test.txt", "w") as f:
+    #     f.write("A file in a subdirectory.")
+    # with open("../static_test/sub/nested_sub/deep.txt", "w") as f:
+    #     f.write("A file in a deeply nested subdirectory.")
 
     # Create an initial 'public_test' to see it get deleted
-    if not os.path.exists("../public_test"):
-        os.makedirs("../public_test/old_files")
-        with open("../public_test/old_files/old.txt", "w") as f:
-            f.write("This file should be deleted.")
+    # if not os.path.exists("../public_test"):
+    #     os.makedirs("../public_test/old_files")
+    #     with open("../public_test/old_files/old.txt", "w") as f:
+    #         f.write("This file should be deleted.")
     
     # copy_directory_contents_clean("static_test", "public_test")
 
@@ -128,4 +120,3 @@ if __name__ == "__main__":
     # print(f"Contents of 'public_test/img': {os.listdir('public_test/img')}")
     # print(f"Contents of 'public_test/sub': {os.listdir('public_test/sub')}")
     # print(f"Contents of 'public_test/sub/nested_sub': {os.listdir('public_test/sub/nested_sub')}")
-    main()
